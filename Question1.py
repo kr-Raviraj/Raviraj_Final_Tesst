631d96bc7c71ae58b0e147bda44f14b0bf6bbd9f
Bharat B <mnsh06002@gmail.com>
2019-11-25 14:56:28 -0500

    Commit of Sql question

--- Question1.py
+++ Question1.py
@@ -1,7 +1,7 @@
 # Write a program in python that takes the matrix of 3*3 and allows the user to enter the number like 0 or 1. Make sure if user wants to start first they will fill the random position on matrix with 0 and the next turn would be taken randomly by computer to fill the spot:
 # a. No Spot should overlap
 # b. Make sure to return (-1) if the matrix if full
-#
+
 import random
 length = 3
 numOnes = 5
