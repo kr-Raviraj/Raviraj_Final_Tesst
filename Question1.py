8647570aab77455dcfb3a823b8de4838e8178159
Bharat B <mnsh06002@gmail.com>
2019-11-25 14:42:45 -0500

    Commit of 1st question

--- Question1.py
+++ Question1.py
@@ -2,4 +2,11 @@
 # a. No Spot should overlap
 # b. Make sure to return (-1) if the matrix if full
 #
-
+import random
+length = 3
+numOnes = 5
+while True:
+    board = [[(random.random() < float(numOnes)/(length**2))*1 for x in range(length)] for x in range(length)]
+    if sum([subarr.count(1) for subarr in board]) == 5:
+        break
+print(board)
