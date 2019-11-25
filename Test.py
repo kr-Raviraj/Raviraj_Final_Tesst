7ae35db85e3d6ac82efa75202cf4f5e3ccad12e5
Raviraj Singh <raviraj.s@consultadd.com>
2019-11-25 17:33:06 -0500

    Sql All querry Done

--- Test.py
+++ Test.py
@@ -1,15 +1,3 @@
-#Question:
-"""Write a program in python that takes the matrix of 3*3 and allows the user to enter the number like 0 or 1. Make sure if user wants to start first they will fill the random position on matrix with 0 and the next turn would be taken randomly by computer to fill the spot:
-a. No Spot should overlap
-b. Make sure to return (-1) if the matrix if full
-"""
-
-
-
-
-
-
-
 import random
 from collections import namedtuple
 RandomValue = namedtuple("RandomValue", ("Value", "RowIndex", "ValueIndex"))
@@ -39,40 +27,19 @@ class Matrix():
         outStr = ""
         for i in range(self.rows):
             outStr += 'Row %s = %s\n' % (i + 1, self.matrix[i])
-
         return outStr
-
 
 a = Matrix(3, 3)
 R = 3
 C = 3
 
-    # Initialize matrix
-matrix = []
-print("Enter the entries rowwise:")
-
-    # For user input
+# For user input
 for i in range(R):  # A for loop for row entries
     a = []
     for j in range(C):  # A for loop for column entries
-        if(j%2==0):
-            a.setitem(1, 2, int(input()))
+        if (j % 2 == 0):
+            a.append(1, 2, int(input()))
 
-
-
-
-a.setitem(2, 1, 10)
 print(random_val.RowIndex)
 print(random_val.ValueIndex)
 print(random_val.Value)
-
-
-#Using Random
-"""import random
-length = 3
-numOnes = 5
-while True:
-    board = [[(random.random() < float(numOnes)/(length**2))*1 for x in range(length)] for x in range(length)]
-    if sum([subarr.count(1) for subarr in board]) == 5:
-        break
-print(board)"""
